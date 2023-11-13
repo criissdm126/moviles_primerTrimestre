ba572e220ef6a7b508dafaf2ab7ee32b1d1e4e0e
criissdm126 <cristinademarialopez@gmail.com>
2023-11-13 09:27:03 +0100

    13/11/2023 acabar constraint

---
+++ CreacionMenu/app/src/main/java/com/example/creacionmenu/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.creacionmenu;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu mimenu){
+        getMenuInflater().inflate(R.menu.menu, mimenu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem opcion_menu){
+        int id = opcion_menu.getItemId();
+        if (id==R.id.Opcion_menu_1){
+            Toast.makeText(this, "Opción menú 1", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (id==R.id.Opcion_menu_2){
+            Toast.makeText(this, "Opción menú 2", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (id==R.id.Opcion_menu_3){
+            Toast.makeText(this, "Opción menú 3", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(opcion_menu);
+    }
+}
