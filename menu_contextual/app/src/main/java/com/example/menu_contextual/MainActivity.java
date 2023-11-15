25c3f453bde3b89c75ba7b390984b5f9a6a68937
criissdm126 <cristinademarialopez@gmail.com>
2023-11-15 14:00:37 +0100

    15/11/2023

--- menu_contextual/app/src/main/java/com/example/menu_contextual/MainActivity.java
+++ menu_contextual/app/src/main/java/com/example/menu_contextual/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.menu_contextual;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu mimenu){
+        getMenuInflater().inflate(R.menu.menu_contextual, mimenu);
+
+        return true;
     }
 }
