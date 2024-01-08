2e63f6f49b25b5df6b85952487b78fd108c7d186
criissdm126 <cristinademarialopez@gmail.com>
2024-01-08 09:47:15 +0100

    8/1/2024

--- PruebaFragmentoDinamico/app/src/main/java/com/example/pruebafragmentodinamico/FragmentoInicio.java
+++
@@ -1,6 +0,0 @@
-package com.example.pruebafragmentodinamico;
-
-import android.app.Activity;
-
-public class FragmentoInicio extends Activity {
-}
--- PruebaFragmentoDinamico/app/src/main/java/com/example/pruebafragmentodinamico/MainActivity.java
+++ PruebaFragmentoDinamico/app/src/main/java/com/example/pruebafragmentodinamico/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.pruebafragmentodinamico;
 
+
 import android.os.Bundle;
 import android.view.View;
+;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,33 +14,27 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-
     FragmentTransaction transaction;
-    FragmentoInicio fragmentInicio;
-    FragmentoRojo fragmentRojo;
-    FragmentoVerda fragmentVerde;
+    Fragment fragmentInicio, fragmentRojo, fragmentVerde;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        fragmentInicio = new FragmentoInicio();
-        fragmentRojo = new FragmentoRojo();
-        fragmentVerde = new FragmentoVerda();
-    }
+        fragmentInicio = new FragmentInicio();
+        fragmentRojo = new FragmentRojo();
+        fragmentVerde = new FragmentVerde();
 
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragmentos,fragmentInicio).commit();
+    }
     public void onClick(View view){
         transaction = getSupportFragmentManager().beginTransaction();
-        switch(view.getId()){
-            case R.id.btnRojo:
-                transaction.replace(R.id.contenedeorFragmentos,fragmentRojo).commit();
-                transaction.addToBackStack(null);
-                break;
-            case R.id.btnVerde:
-                transaction.replace(R.id.contenedeorFragmentos,fragmentVerde).commit();
-                transaction.addToBackStack(null);
-                break;
+        if (view.getId() == R.id.btnRojo) {
+            transaction.replace(R.id.contenedorFragmentos,fragmentRojo).commit();
         }
-        transaction.commit();
+        else if (view.getId() == R.id.btnVerde) {
+            transaction.replace(R.id.contenedorFragmentos,fragmentVerde).commit();
+        }
     }
+
+
 }
